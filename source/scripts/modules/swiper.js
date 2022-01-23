7e9c5b8bbebe442fd9c4c894806289c6c3ef358a
Yuriy Bevov <jury19y@mail.ru>
2022-01-23 19:35:12 +0300

    object-card, average

---
+++ source/scripts/modules/swiper.js
@@ -0,0 +1,23 @@
+import Swiper, { Autoplay, Thumbs, Zoom} from 'swiper';
+Swiper.use([Autoplay, Thumbs, Zoom]);
+
+const slider = document.querySelector('.swiper-container');
+
+if(slider) {
+    let swiper = new Swiper(".apartment-slider-thumbs", {
+      slidesPerView: "auto",
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      spaceBetween: 10,
+    });
+
+    let swiperThumbs = new Swiper(".apartment-slider", {
+      zoom: true,
+      loop: true,
+      spaceBetween: 10,
+
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+}
